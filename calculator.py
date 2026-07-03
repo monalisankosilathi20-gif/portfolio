4c7a5f258833a89afc141dd3ab27316239cedd66
monalisankosilathi20-gif <monalisankosilathi20@gmail.com>
2026-07-03 09:27:18 -0300

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,36 @@
+a = 90
+b = 23
+op = "*"
+
+if op == "+":
+    print("Addition =", a + b)
+elif op == "-":
+    print("Subtraction =", a - b)
+elif op == "*":
+    print("Multiplication =", a * b)
+elif op == "/":
+    print("Division =", a / b)
+else:
+    print("Invalid operation")
+
+
+    a = 90
+b = 23
+op = "+"
+
+if op == "+":
+    print(a + b)
+elif op == "-":
+    print(a - b)
+elif op == "*":
+    print(a * b)
+elif op == "/":
+    print(a / b)
+else:
+    print("Invalid operation")
+
+    a = 90
+b = 23
+op = "*"
+
+
---
+++ email.py
@@ -0,0 +1,10 @@
+correct_email = "monalisankosilathi20@gmail.com"
+correct_password = "lovemonalisa20"
+
+email = input("monalisankosilathi20@gmail.com: ")
+password = input("lovemonalisa20: ")
+
+if email == correct_email and password == correct_password:
+    print("Login successful")
+else:
+    print("Incorrect email or password")
